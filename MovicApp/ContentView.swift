b0ae881a8d7a366a068d4a3248b03ab13df68fa5
Keigo <aporo967@gmail.com>
2020-08-01 15:00:23 +0900

    Initial Commit

---
+++ MovicApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MovicApp
+//
+//  Created by け on 2020/08/01.
+//  Copyright © 2020 ke. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
